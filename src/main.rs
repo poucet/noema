44d4a411a3c7fb81588fc97985713e8171339e09
Chris Poucet <christophe.poucet@gmail.com>
2025-10-17 10:44:13 +0200

    Made streaming work for Gemini and Ollama.
    TODO: Clean up boilerplate code.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use clap::Parser;
-use llm::{ChatModel, ModelProvider};
+use llm::{ChatModel, ChatRequest, ModelProvider};
 use llm::providers::{GeminiProvider, GeneralModelProvider};
 use llm::providers::OllamaProvider;
 use futures::{StreamExt};
-use tokio::pin;
 
 use clap_derive::{Parser, ValueEnum};
 use dotenv;
@@ -27,27 +26,33 @@ enum ModelProviderType {
     Gemini,
 }
 
+#[derive(Clone, ValueEnum, Debug, PartialEq, Eq)]
+#[clap(rename_all = "lowercase")]
+enum Mode {
+    Chat,
+    Stream,
+}
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(long, value_enum, default_value_t = ModelProviderType::Gemini)]
     model: ModelProviderType,
+
+    #[arg(long, value_enum, default_value_t = Mode::Chat)]
+    mode: Mode,
 }
 
 async fn call_model_regular(model: &dyn ChatModel, messages: Vec<llm::ChatMessage>) -> anyhow::Result<()> {
-    let response = model.chat(messages).await?;
+    let request = ChatRequest::new(messages);
+    let response = model.chat(&request).await?;
     println!("Response: {:}", response.content);
     Ok(())
 }
 
-async fn call_model_streaming(model: &impl ChatModel) -> anyhow::Result<()> {
-    let messages = vec![
-        llm::ChatMessage {
-            role: llm::Role::User,
-            content: "Hello, model!".to_string(),
-        },
-    ];
-    let mut stream = model.stream_chat(messages).await?;
+async fn call_model_streaming(model: &impl ChatModel, messages: Vec<llm::ChatMessage> ) -> anyhow::Result<()> {
+    let request = ChatRequest::new(messages);
+    let mut stream = model.stream_chat(&request).await?;
     print!("Response: ");
     while let Some(chunk) = stream.next().await {
         print!("{:}", chunk.content);
@@ -63,15 +68,22 @@ async fn main() {
         ModelProviderType::Ollama => GeneralModelProvider::Ollama(OllamaProvider::default()),
         ModelProviderType::Gemini => GeneralModelProvider::Gemini(GeminiProvider::default(&get_gemini_api_key())),
     };
+    let model_name = match args.model {
+         ModelProviderType::Ollama => "gemma3n:latest",
+         ModelProviderType::Gemini => "models/gemini-2.5-flash",
+    };
     let models = provider.list_models().await;
     println!("Available models: {:?}", models);
 
-    let model = provider.create_chat_model("models/gemini-2.5-flash").unwrap();
+    let model = provider.create_chat_model(model_name).unwrap();
     let messages = vec![
         llm::ChatMessage {
             role: llm::Role::User,
-            content: "Hello, Ollama!".to_string(),
+            content: "Hello, please return a long meaningful message!".to_string(),
         },
     ];
-    call_model_regular(&model, messages).await.unwrap();
+    match args.mode {
+        Mode::Chat => call_model_regular(&model, messages).await.unwrap(),
+        Mode::Stream => call_model_streaming(&model, messages).await.unwrap()
+    }
 }
